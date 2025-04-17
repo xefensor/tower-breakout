50d46ce6d6a9c910bf237e155e23ba0d4e17589a
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-04-17 16:12:46 +0200

    paddle update

--- paddles/paddle.gd
+++ paddles/paddle.gd
@@ -3,18 +3,18 @@ class_name Paddle
 
 
 @export var speed : float = 5
+@onready var _area_2d : Area2D = NodeUtils.get_child_by_class(self, Area2D)
+
+
+func _ready() -> void:
+	_area_2d.body_entered.connect(_on_area_2d_body_entered)
 
 
 func _physics_process(delta: float) -> void:
 	var _direction = Input.get_axis("paddle_left", "paddle_right")
 	var _velocity = Vector2(_direction * speed, 0)
 	
-	var collision_info = move_and_collide(_velocity * delta)
-	if collision_info:
-		var collider = collision_info.get_collider()
-		if collider is Ball:
-			var ball = collider
-			_on_ball_hit(ball, collision_info.get_position())
+	var _collision_info = move_and_collide(_velocity * delta)
 			
 			
 func _on_ball_hit(ball: Ball, position: Vector2) -> void:
@@ -25,3 +25,8 @@ func _on_ball_hit(ball: Ball, position: Vector2) -> void:
 
 	var angle = deg_to_rad(85 * relative_hit_pos)  # -85° až 85°
 	ball.velocity = Vector2(sin(angle), -cos(angle)).normalized() * ball.speed
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Ball:
+		_on_ball_hit(body, body.global_position)
