152ae1df139f2acd2bf8aab53f8d1b3470de5431
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-04-19 02:51:18 +0200

    background

---
+++ levels/background.gd
@@ -0,0 +1,10 @@
+extends TextureRect
+
+func _ready():
+	get_viewport().size_changed.connect(_on_size_changed)
+	expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	stretch_mode = TextureRect.STRETCH_TILE
+	_on_size_changed()
+
+func _on_size_changed():
+	size = get_viewport().get_visible_rect().size
