45d62c1674a93f8635ea99a1f919acf8ae63080f
GINZUcz <GINZUcz@seznam.cz>
2025-07-22 20:23:51 +0200

    Upgrade Paddle Speed

--- levels/level_ui.gd
+++ levels/level_ui.gd
@@ -2,6 +2,13 @@ class_name LevelUI
 extends CanvasLayer
 
 @export var money_label: Label
+@onready var paddle = $"../Paddle"
 
 func update_money_label(new_val: int):
 	money_label.text = str(new_val)
+
+
+func _on_upgrade_paddle_speed_pressed() -> void:
+	if Level.instance.money >= 5:
+		paddle._speed += 100
+		Level.instance.money -= 5
