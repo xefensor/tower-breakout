301fd97d20b2472c9280c6ca8e5d6e2d38654ac9
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-07-24 18:37:40 +0200

    Revert "Upgrade Paddle Speed"
    This reverts commit 45d62c1674a93f8635ea99a1f919acf8ae63080f.

--- paddles/paddle.gd
+++ paddles/paddle.gd
@@ -9,7 +9,7 @@ extends AnimatableBody2D
 @onready var default_health: int = health.current_health
 @onready var _area_2d: Area2D = NodeUtils.get_child_by_class(self, Area2D)
 @onready var collision_shape_2d: CollisionShape2D = NodeUtils.get_child_by_class(self, CollisionShape2D)
-@onready var level_ui: LevelUI = NodeUtils.get_child_by_class(self, LevelUI)
+
 
 func _ready() -> void:
 	_area_2d.body_entered.connect(_on_area_2d_body_entered)
