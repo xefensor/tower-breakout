ad390a1a2ee26384e8d1e80291eaf9da84708826
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-11-05 08:59:42 +0100

    Update cannon_bounce.gd

--- balls/bounces/cannon_bounce.gd
+++ balls/bounces/cannon_bounce.gd
@@ -2,8 +2,7 @@ class_name CannonBounce
 extends Bounce
 
 
-func calculate_bounce(ball: Ball, collision_info: KinematicCollision2D,
- _hitted_enemy: bool) -> Vector2:
+func calculate_bounce(ball: Ball, collision_info: KinematicCollision2D) -> Vector2:
 	var normal: Vector2 = collision_info.get_normal()
 	var bounce: Vector2 = ball.velocity.bounce(normal)
 	if collision_info.get_collider().is_queued_for_deletion():
